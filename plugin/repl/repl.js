c647f04d4fe305ae34180d036fd165ac6a9668aa
Bodil Stokke <bodil@bodil.org>
2013-10-20 02:38:25 +0200

    Licensing.

--- plugin/repl/repl.js
+++ plugin/repl/repl.js
@@ -1,6 +1,12 @@
 /*global Reveal:true, window:true, document:true, setTimeout:true,
   ace:true, console:true, socket, io */
 
+/* Copyright 2013 Bodil Stokke <bodil@bodil.org>
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
 (function() {
 
   var comments = {
--- run.js
+++ run.js
@@ -1,5 +1,11 @@
 #!/usr/bin/env node
 
+/* Copyright 2013 Bodil Stokke <bodil@bodil.org>
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
 var fs = require("fs");
 var path = require("path");
 var express = require("express");
